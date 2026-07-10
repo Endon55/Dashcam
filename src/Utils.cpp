53f19dc1b85a3412cae3729e57fa8b680f917b72
Anthony <anthony@anthonycosenza.com>
2026-07-10 08:27:23 -0600

    few more bug fixes

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -30,9 +30,21 @@ const char* Utils::str_or_default(const char* str, const char* def)
 
 void Utils::log_cap_mode(const capture_mode* mode)
 {
-    spdlog::debug("Format: {}", mode->pixelFmtStr);
-    spdlog::debug("FPS: {}", mode->fps);
-    spdlog::debug("Resolution: {}x{}", mode->width, mode->height);
+    if(mode)
+    {
+        if(mode->pixelFmtStr)
+        {
+            spdlog::debug("Format: {}", mode->pixelFmtStr);
+        }
+        if(mode->fps)
+        {
+            spdlog::debug("FPS: {}", mode->fps);
+        }
+        if(mode->width && mode->height)
+        {
+            spdlog::debug("Resolution: {}x{}", mode->width, mode->height);
+        }
+    }
 }
 
 const char* Utils::resolution_to_str(int width, int height)
